e86fe082fbc9b9367439ddbabaa5a18fbd87d10c
dikshantpradhan <dikshantpradhan@gmail.com>
2016-03-09 03:18:54 -0600

    Almost finished

---
+++ Almost Final/regionData.js
@@ -0,0 +1,268 @@
+var jsonData = [
+    {
+     "model": "Tweets.region", "pk": 1, "fields": {
+        "location": "40.713,-74.007,2km", "subunit": 0, "pulled": true, "tweetScore": 17.09999999999999, "finalScore": 24.259999999999987, "adjacent": [2, 3]}
+    },
+    {
+     "model": "Tweets.region", "pk": 2, "fields": {
+        "location": "40.719,-73.987,3km", "subunit": 97, "pulled": true, "tweetScore": 17.69999999999999, "finalScore": 32.759999999999984, "adjacent": [1, 3, 4, 5]}
+    },
+    {
+     "model": "Tweets.region", "pk": 3, "fields": {
+        "location": "40.731,-74,2km", "subunit": 139, "pulled": true, "tweetScore": 18.099999999999998, "finalScore": 29.019999999999992, "adjacent": [1, 2, 6]}
+    },
+    {
+     "model": "Tweets.region", "pk": 4, "fields": {
+        "location": "40.747,-74.002,3km", "subunit": 104, "pulled": true, "tweetScore": 20.59999999999999, "finalScore": 31.599999999999987, "adjacent": [2, 5, 7]}
+    },
+    {
+     "model": "Tweets.region", "pk": 5, "fields": {
+        "location": "40.756,-73.983,2km", "subunit": 105, "pulled": true, "tweetScore": 19.49999999999999, "finalScore": 37.33999999999998, "adjacent": [2, 4, 6, 8, 61]}
+    },
+    {
+     "model": "Tweets.region", "pk": 6, "fields": {
+        "location": "40.75,-73.97,2km", "subunit": 4, "pulled": true, "tweetScore": 19.799999999999986, "finalScore": 30.779999999999983, "adjacent": [3, 5, 8]}
+    },
+    {
+     "model": "Tweets.region", "pk": 7, "fields": {
+        "location": "40.787,-73.976,2.5km", "subunit": 121, "pulled": true, "tweetScore": 17.799999999999994, "finalScore": 27.07999999999999, "adjacent": [4, 9, 60, 61]}
+    },
+    {
+     "model": "Tweets.region", "pk": 8, "fields": {
+        "location": "40.771,-73.958,2km", "subunit": 8, "pulled": true, "tweetScore": 17.299999999999997, "finalScore": 31.69999999999999, "adjacent": [5, 6, 11, 60, 61]}
+    },
+    {
+     "model": "Tweets.region", "pk": 9, "fields": {
+        "location": "40.813,-73.957,2km", "subunit": 136, "pulled": true, "tweetScore": 11.799999999999997, "finalScore": 20.539999999999996, "adjacent": [7, 9, 10, 12]}
+    },
+    {
+     "model": "Tweets.region", "pk": 10, "fields": {
+        "location": "40.809,-73.946,2km", "subunit": 137, "pulled": true, "tweetScore": 10.499999999999998, "finalScore": 20.479999999999997, "adjacent": [9, 11, 12, 16, 61]}
+    },
+    {
+     "model": "Tweets.region", "pk": 11, "fields": {
+        "location": "40.795,-73.932,4km", "subunit": 9, "pulled": true, "tweetScore": 18.699999999999985, "finalScore": 29.019999999999985, "adjacent": [8, 10, 13, 61]}
+    },
+    {
+     "model": "Tweets.region", "pk": 12, "fields": {
+        "location": "40.844,-73.937,2km", "subunit": 134, "pulled": true, "tweetScore": 3.6000000000000005, "finalScore": 9.52, "adjacent": [9, 10, 16, 17, 19, 20, 60]}
+    },
+    {
+     "model": "Tweets.region", "pk": 13, "fields": {
+        "location": "40.812,-73.915,3km", "subunit": 122, "pulled": true, "tweetScore": 9.999999999999998, "finalScore": 15.799999999999995, "adjacent": [11, 14, 15, 16]}
+    },
+    {
+     "model": "Tweets.region", "pk": 14, "fields": {
+        "location": "40.813,-73.886,3km", "subunit": 11, "pulled": true, "tweetScore": 1.9, "finalScore": 5.9, "adjacent": [13, 15, 21]}
+    },
+    {
+     "model": "Tweets.region", "pk": 15, "fields": {
+        "location": "40.838,-73.893,3km", "subunit": 94, "pulled": true, "tweetScore": 6.400000000000001, "finalScore": 9.900000000000002, "adjacent": [13, 14, 16, 18, 21]}
+    },
+    {
+     "model": "Tweets.region", "pk": 16, "fields": {
+        "location": "40.843,-73.915,2km", "subunit": 138, "pulled": true, "tweetScore": 2.0, "finalScore": 8.5, "adjacent": [10, 12, 13, 15, 17]}
+    },
+    {
+     "model": "Tweets.region", "pk": 17, "fields": {
+        "location": "40.853,-73.911,2km", "subunit": 98, "pulled": true, "tweetScore": 2.0, "finalScore": 3.4400000000000004, "adjacent": [12, 16, 18, 19]}
+    },
+    {
+     "model": "Tweets.region", "pk": 18, "fields": {
+        "location": "40.949,-73.893,2.5km", "subunit": 113, "pulled": true, "tweetScore": 0.0, "finalScore": 3.4200000000000004, "adjacent": [15, 17, 19, 21, 62]}
+    },
+    {
+     "model": "Tweets.region", "pk": 19, "fields": {
+        "location": "40.862,-73.896,2km", "subunit": 110, "pulled": true, "tweetScore": 1.6, "finalScore": 4.12, "adjacent": [12, 17, 18, 20, 24, 60, 62]}
+    },
+    {
+     "model": "Tweets.region", "pk": 20, "fields": {
+        "location": "40.884,-73.906,3km", "subunit": 135, "pulled": true, "tweetScore": 1.5, "finalScore": 2.58, "adjacent": [12, 19, 60]}
+    },
+    {
+     "model": "Tweets.region", "pk": 21, "fields": {
+        "location": "40.827,-73.86,3km", "subunit": 123, "pulled": true, "tweetScore": 3.5999999999999996, "finalScore": 6.9799999999999995, "adjacent": [14, 15, 18, 22, 23, 62]}
+    },
+    {
+     "model": "Tweets.region", "pk": 22, "fields": {
+        "location": "40.824,-73.827,4km", "subunit": 148, "pulled": true, "tweetScore": 2.5, "finalScore": 4.76, "adjacent": [21, 23, 24, 63]}
+    },
+    {
+     "model": "Tweets.region", "pk": 23, "fields": {
+        "location": "40.857,-73.855,3km", "subunit": 114, "pulled": true, "tweetScore": 2.5999999999999996, "finalScore": 4.879999999999999, "adjacent": [21, 22, 24, 62]}
+    },
+    {
+     "model": "Tweets.region", "pk": 24, "fields": {
+        "location": "40.884,-73.85,4km", "subunit": 106, "pulled": true, "tweetScore": 1.8, "finalScore": 4.54, "adjacent": [19, 22, 23, 60, 62, 63]}
+    },
+    {
+     "model": "Tweets.region", "pk": 25, "fields": {
+        "location": "40.715,-73.945,3km", "subunit": 130, "pulled": true, "tweetScore": 19.69999999999999, "finalScore": 37.97999999999998, "adjacent": [26, 27, 28, 43, 44, 47]}
+    },
+    {
+     "model": "Tweets.region", "pk": 26, "fields": {
+        "location": "40.684,-73.981,2km", "subunit": 140, "pulled": true, "tweetScore": 13.499999999999996, "finalScore": 23.619999999999994, "adjacent": [25, 27, 30, 32]}
+    },
+    {
+     "model": "Tweets.region", "pk": 27, "fields": {
+        "location": "40.688,-73.949,3km", "subunit": 90, "pulled": true, "tweetScore": 18.79999999999999, "finalScore": 29.19999999999999, "adjacent": [25, 26, 28, 32, 40]}
+    },
+    {
+     "model": "Tweets.region", "pk": 28, "fields": {
+        "location": "40.699,-73.921,2km", "subunit": 131, "pulled": true, "tweetScore": 9.0, "finalScore": 23.899999999999995, "adjacent": [25, 27, 29, 40, 47, 61]}
+    },
+    {
+     "model": "Tweets.region", "pk": 29, "fields": {
+        "location": "40.667,-73.886,4km", "subunit": 101, "pulled": true, "tweetScore": 7.4, "finalScore": 15.04, "adjacent": [28, 40, 42, 47, 51, 52]}
+    },
+    {
+     "model": "Tweets.region", "pk": 30, "fields": {
+        "location": "40.677,-73.997,2km", "subunit": 107, "pulled": true, "tweetScore": 3.300000000000001, "finalScore": 10.6, "adjacent": [26, 31, 32, 64]}
+    },
+    {
+     "model": "Tweets.region", "pk": 31, "fields": {
+        "location": "40.646,-74.003,3km", "subunit": 117, "pulled": true, "tweetScore": 4.6, "finalScore": 8.440000000000001, "adjacent": [30, 34, 36, 64]}
+    },
+    {
+     "model": "Tweets.region", "pk": 32, "fields": {
+        "location": "40.673,-73.95,2km", "subunit": 95, "pulled": true, "tweetScore": 8.8, "finalScore": 20.72, "adjacent": [26, 27, 30, 33, 40, 41, 64]}
+    },
+    {
+     "model": "Tweets.region", "pk": 33, "fields": {
+        "location": "40.662,-73.946,2km", "subunit": 127, "pulled": true, "tweetScore": 7.800000000000003, "finalScore": 12.840000000000003, "adjacent": [32, 38, 40, 41, 64]}
+    },
+    {
+     "model": "Tweets.region", "pk": 34, "fields": {
+        "location": "40.627,-73.018,3km", "subunit": 118, "pulled": true, "tweetScore": 0.0, "finalScore": 2.22, "adjacent": [31, 35, 36]}
+    },
+    {
+     "model": "Tweets.region", "pk": 35, "fields": {
+        "location": "40.609,-73.998,3km", "subunit": 91, "pulled": true, "tweetScore": 0.2, "finalScore": 1.5400000000000003, "adjacent": [34, 36, 37, 39]}
+    },
+    {
+     "model": "Tweets.region", "pk": 36, "fields": {
+        "location": "40.629,-73.991,3.5km", "subunit": 112, "pulled": true, "tweetScore": 6.300000000000001, "finalScore": 7.340000000000001, "adjacent": [31, 34, 35, 38, 39]}
+    },
+    {
+     "model": "Tweets.region", "pk": 37, "fields": {
+        "location": "40.582,-73.966,2.5km", "subunit": 111, "pulled": true, "tweetScore": 0.2, "finalScore": 0.28, "adjacent": [35, 39]}
+    },
+    {
+     "model": "Tweets.region", "pk": 38, "fields": {
+        "location": "40.614,-73.953,2.5km", "subunit": 119, "pulled": true, "tweetScore": 0.2, "finalScore": 7.980000000000001, "adjacent": [33, 36, 39, 41, 42, 64]}
+    },
+    {
+     "model": "Tweets.region", "pk": 39, "fields": {
+        "location": "40.598,-73.945,2.5km", "subunit": 89, "pulled": true, "tweetScore": 0.2, "finalScore": 3.4600000000000004, "adjacent": [35, 36, 37, 38, 42]}
+    },
+    {
+     "model": "Tweets.region", "pk": 40, "fields": {
+        "location": "40.657,-73.915,2km", "subunit": 103, "pulled": true, "tweetScore": 1.0, "finalScore": 14.36, "adjacent": [27, 28, 29, 32, 33, 41, 42]}
+    },
+    {
+     "model": "Tweets.region", "pk": 41, "fields": {
+        "location": "40.639,-73.935,2km", "subunit": 108, "pulled": true, "tweetScore": 5.600000000000001, "finalScore": 11.040000000000003, "adjacent": [32, 33, 38, 40, 42]}
+    },
+    {
+     "model": "Tweets.region", "pk": 42, "fields": {
+        "location": "40.634,-73.913,4km", "subunit": 17, "pulled": true, "tweetScore": 9.4, "finalScore": 12.420000000000002, "adjacent": [29, 38, 39, 40, 41, 67]}
+    },
+    {
+     "model": "Tweets.region", "pk": 43, "fields": {
+        "location": "40.76,-73.919,4km", "subunit": 42, "pulled": true, "tweetScore": 15.299999999999995, "finalScore": 24.639999999999993, "adjacent": [25, 44, 45]}
+    },
+    {
+     "model": "Tweets.region", "pk": 44, "fields": {
+        "location": "40.741,-73.927,4km", "subunit": 102, "pulled": true, "tweetScore": 20.999999999999993, "finalScore": 32.51999999999999, "adjacent": [25, 43, 45, 46, 47]}
+    },
+    {
+     "model": "Tweets.region", "pk": 45, "fields": {
+        "location": "40.756,-73.877,3km", "subunit": 96, "pulled": true, "tweetScore": 6.0, "finalScore": 14.039999999999997, "adjacent": [43, 44, 46, 65]}
+    },
+    {
+     "model": "Tweets.region", "pk": 46, "fields": {
+        "location": "40.748,-73.867,2.5km", "subunit": 92, "pulled": true, "tweetScore": 2.8, "finalScore": 11.959999999999997, "adjacent": [44, 45, 47, 48, 65]}
+    },
+    {
+     "model": "Tweets.region", "pk": 47, "fields": {
+        "location": "40.721,-73.889,4km", "subunit": 99, "pulled": true, "tweetScore": 13.799999999999999, "finalScore": 26.559999999999995, "adjacent": [25, 28, 29, 44, 46, 48]}
+    },
+    {
+     "model": "Tweets.region", "pk": 48, "fields": {
+        "location": "40.733,-73.856,2.5km", "subunit": 109, "pulled": true, "tweetScore": 3.9000000000000004, "finalScore": 7.84, "adjacent": [46, 47, 51, 65]}
+    },
+    {
+     "model": "Tweets.region", "pk": 49, "fields": {
+        "location": "40.776,-73.821,4km", "subunit": 124, "pulled": true, "tweetScore": 4.6000000000000005, "finalScore": 6.620000000000001, "adjacent": [50, 53, 65]}
+    },
+    {
+     "model": "Tweets.region", "pk": 50, "fields": {
+        "location": "40.739,-73.812,4km", "subunit": 125, "pulled": true, "tweetScore": 6.7, "finalScore": 11.18, "adjacent": [49, 51, 53, 54, 55, 65]}
+    },
+    {
+     "model": "Tweets.region", "pk": 51, "fields": {
+        "location": "40.697,-73.832,2km", "subunit": 141, "pulled": true, "tweetScore": 2.0, "finalScore": 7.66, "adjacent": [29, 48, 50, 52, 54, 65]}
+    },
+    {
+     "model": "Tweets.region", "pk": 52, "fields": {
+        "location": "40.67,-73.824,3km", "subunit": 48, "pulled": true, "tweetScore": 3.0, "finalScore": 7.3, "adjacent": [29, 51, 53, 66]}
+    },
+    {
+     "model": "Tweets.region", "pk": 53, "fields": {
+        "location": "40.761,-73.769,4km", "subunit": 120, "pulled": true, "tweetScore": 2.3000000000000003, "finalScore": 7.640000000000001, "adjacent": [49, 50, 52, 54, 55]}
+    },
+    {
+     "model": "Tweets.region", "pk": 54, "fields": {
+        "location": "40.692,-73.78,4km", "subunit": 100, "pulled": true, "tweetScore": 6.2, "finalScore": 9.64, "adjacent": [50, 51, 53, 55]}
+    },
+    {
+     "model": "Tweets.region", "pk": 55, "fields": {
+        "location": "40.698,-73.745,5km", "subunit": 133, "pulled": true, "tweetScore": 6.2, "finalScore": 11.2, "adjacent": [50, 53, 54, 66]}
+    },
+    {
+     "model": "Tweets.region", "pk": 56, "fields": {
+        "location": "40.591,-73.799,4km", "subunit": 50, "pulled": true, "tweetScore": 2.2, "finalScore": 2.2, "adjacent": []}
+    },
+    {
+     "model": "Tweets.region", "pk": 57, "fields": {
+        "location": "40.616,-74.132,5km", "subunit": 75, "pulled": true, "tweetScore": 5.500000000000001, "finalScore": 6.360000000000001, "adjacent": [58]}
+    },
+    {
+     "model": "Tweets.region", "pk": 58, "fields": {
+        "location": "40.582,-74.152,6km", "subunit": 88, "pulled": true, "tweetScore": 4.300000000000001, "finalScore": 6.1000000000000005, "adjacent": [57, 59]}
+    },
+    {
+     "model": "Tweets.region", "pk": 59, "fields": {
+        "location": "40.537,-74.203,8km", "subunit": 84, "pulled": true, "tweetScore": 3.5, "finalScore": 4.36, "adjacent": [58]}
+    },
+    {
+     "model": "Tweets.region", "pk": 60, "fields": {
+        "location": "40.896,-73.885,2km", "subunit": 93, "pulled": true, "tweetScore": 0.2, "finalScore": 8.919999999999998, "adjacent": [7, 8, 12, 19, 20, 24]}
+    },
+    {
+     "model": "Tweets.region", "pk": 61, "fields": {
+        "location": "40.783,-73.966,2km", "subunit": 129, "pulled": true, "tweetScore": 13.799999999999994, "finalScore": 32.359999999999985, "adjacent": [5, 7, 8, 10, 11, 28]}
+    },
+    {
+     "model": "Tweets.region", "pk": 62, "fields": {
+        "location": "40.856,-73.876,2km", "subunit": 115, "pulled": true, "tweetScore": 3.5, "finalScore": 5.42, "adjacent": [18, 19, 21, 23, 24]}
+    },
+    {
+     "model": "Tweets.region", "pk": 63, "fields": {
+        "location": "40.78,-73.806,3km", "subunit": 14, "pulled": true, "tweetScore": 3.3000000000000003, "finalScore": 4.16, "adjacent": [22, 24]}
+    },
+    {
+     "model": "Tweets.region", "pk": 64, "fields": {
+        "location": "40.662,-73.967,2km", "subunit": 128, "pulled": true, "tweetScore": 9.6, "finalScore": 14.540000000000001, "adjacent": [30, 31, 32, 33, 38]}
+    },
+    {
+     "model": "Tweets.region", "pk": 65, "fields": {
+        "location": "40.74,-73.84,2km", "subunit": 126, "pulled": true, "tweetScore": 1.1, "finalScore": 6.300000000000001, "adjacent": [45, 46, 48, 49, 50, 51]}
+    },
+    {
+     "model": "Tweets.region", "pk": 66, "fields": {
+        "location": "40.65,-73.784,5km", "subunit": 56, "pulled": true, "tweetScore": 9.8, "finalScore": 11.64, "adjacent": [52, 55]}
+    },
+    {
+     "model": "Tweets.region", "pk": 67, "fields": {
+        "location": "40.594,-73.9,3km", "subunit": 38, "pulled": true, "tweetScore": 0.7, "finalScore": 2.58, "adjacent": [42]}}]
